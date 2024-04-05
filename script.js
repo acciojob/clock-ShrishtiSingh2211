34232c11cb377be96cb51426f38d6a213c6f36ee
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-04-05 13:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateTimer() {
+    const timerElement = document.getElementById('timer');
+    const now = new Date();
+    const dateStr = now.toDateString();
+    const timeStr = now.toLocaleTimeString();
+    timerElement.textContent = `${dateStr} ,${timeStr}`;
+}
+
+// Call updateTimer function initially to display the timer
+updateTimer();
+
+// Update the timer every second
+setInterval(updateTimer, 1000);
